a86f82193d667e62f29c446a8c9aafa11da98ad7
koushik.pathuri <119720431+pathurikoushik@users.noreply.github.com>
2023-09-17 15:29:15 +0530

    def prime(a):     if a==1:         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if prime(i):         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,14 @@
+def prime(a):
+    if a==1:
+        return 0
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if prime(i):
+        c+=1
+print(c)
